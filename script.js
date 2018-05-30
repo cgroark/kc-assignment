72367c6d3aaf52e44194850caac518ca547324a1
cgroark <colingroark@gmail.com>
2018-05-29 21:42:11 -0700

    built out programs section

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-let accordion = document.getElementsByClassName('accordion');
-var i;
+let accordion = $('.accordion');
+let accordionMini = $('.accordion-mini');
+let video = $('.career-video');
+console.log(accordion)
+console.log(video)
+let i;
 
-for(i=0; i<accordion.length; i++){
+$('.video-button').on( 'click', function() {
+ 	$('.career-video').css('display', 'inline-block');
+});
+
+$('.hide-button').on('click', function(){
+	console.log('function reachedddd')
+	$('.career-video').css('display', 'none');
+});
+
+
+for(i=0; i<accordion.length; i++) {
 	accordion[i].addEventListener('click', function(){
 		this.classList.toggle('active');
 
@@ -13,3 +27,16 @@ for(i=0; i<accordion.length; i++){
 		}
 	});
 }
+
+for(i=0; i<accordionMini.length; i++){
+	accordionMini[i].addEventListener('click', function(){
+		this.classList.toggle('active-mini');
+		var panel = this.nextElementSibling;
+		if(panel.style.display === 'block') {
+			panel.style.display = 'none';
+		}else{
+			panel.style.display = 'block';
+		}
+	});
+}
+
